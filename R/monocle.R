3dbc03f0ac69ff4391def653e09197ebd3bf5106
whtns <kevin.stachelek@gmail.com>
2020-07-31 07:47:37 -0700

    update monocle heatmap

--- R/monocle.R
+++ R/monocle.R
@@ -668,20 +668,33 @@ plot_cells <- function(cds, x = 1, y = 2, reduction_method = c("UMAP", "tSNE",
   g
 }
 
+
 #' Title
 #'
 #' @param cds
+#' @param cells
 #' @param pr_deg_ids
+#' @param seu_resolution
 #' @param collapse_rows
 #' @param collapse_cols
-#' @param seu_resolution
+#' @param resolution
+#' @param group.by
+#' @param group.bar.height
+#' @param cluster_columns
+#' @param column_split
+#' @param col_dendrogram
+#' @param mm_col_dend
+#' @param ...
 #'
 #' @return
 #' @export
 #'
 #' @examples
-monocle_module_heatmap <- function(cds, pr_deg_ids, seu_resolution, collapse_rows = TRUE, collapse_cols = TRUE, resolution = 10^seq(-6,-1)) {
-
+monocle_module_heatmap <- function(cds, pr_deg_ids, seu_resolution, cells = NULL, collapse_rows = TRUE,
+                                       resolution = 10^seq(-6,-1), group.by = "batch", group.bar.height = 0.01,
+                                       cluster_columns = FALSE, cluster_rows = FALSE,
+                                   column_split = NULL, col_dendrogram = "ward.D2", mm_col_dend = 30, ...){
+  browser()
   if (any(grepl("integrated", colnames(cds@colData)))){
     default_assay = "integrated"
   } else {
@@ -694,37 +707,72 @@ monocle_module_heatmap <- function(cds, pr_deg_ids, seu_resolution, collapse_row
   gene_module_df <- monocle3::find_gene_modules(cds, resolution=resolution) %>%
     dplyr::arrange(module)
 
+  cell_group_df <- tibble::tibble(cell=row.names(colData(cds)),
+                                  cell_group=colData(cds)[[seu_resolution]]) %>%
+    dplyr::mutate(cell_group = cell)
+
   if(collapse_rows != TRUE){
-    gene_module_df <-
+    heatmap_row_df <-
       dplyr::select(gene_module_df, id) %>%
       dplyr::mutate(module = id)
+
+    row_ha <- ComplexHeatmap::rowAnnotation(module = gene_module_df$module)
+  } else{
+    heatmap_row_df <- gene_module_df
+
+    row_ha <- ComplexHeatmap::rowAnnotation(module = unique(gene_module_df$module))
   }
 
-  cell_group_df <- tibble::tibble(cell=row.names(colData(cds)),
-                                  cell_group=colData(cds)[[seu_resolution]])
+  agg_mat <- monocle3::aggregate_gene_expression(cds, heatmap_row_df)
 
-  if (collapse_cols != TRUE){
-    cell_group_df <- dplyr::mutate(cell_group_df, cell_group = cell)
+  group.by <- group.by %||% "batch"
+  cells <- cells %||% colnames(x = cds)
+
+  pseudotime_tbl <-
+    monocle3::pseudotime(cds) %>%
+    tibble::enframe("sample_id", "pseudotime")
+
+  groups.use <- colData(cds)[cells, group.by, drop = FALSE] %>%
+    as.data.frame() %>% tibble::rownames_to_column("sample_id") %>%
+    dplyr::mutate(across(where(is.character), as.factor)) %>%
+    dplyr::left_join(pseudotime_tbl, by = "sample_id") %>%
+    dplyr::arrange(pseudotime) %>%
+    data.frame(row.names = 1) %>% identity()
+
+  groups.use.factor <- groups.use[sapply(groups.use, is.factor)]
+  ha_cols.factor <- NULL
+  if (length(groups.use.factor) > 0) {
+    ha_col_names.factor <- lapply(groups.use.factor, levels)
+    ha_cols.factor <- purrr::map(ha_col_names.factor, ~(scales::hue_pal())(length(.x))) %>%
+      purrr::map2(ha_col_names.factor, set_names)
   }
-
-  agg_mat <- monocle3::aggregate_gene_expression(cds, gene_module_df, cell_group_df)
-
-  if (dim(agg_mat)[2] > 20){
-    col_order <- sort(monocle3::pseudotime(cds))
-    agg_mat <- agg_mat[,names(col_order)]
-
-    module_heatmap <- iheatmapr::iheatmap(as.matrix(agg_mat), col_labels = TRUE, row_labels = TRUE, cluster_rows = "hclust", cluster_cols = NULL) %>%
-      # add_col_annotation(data.frame("Groups" = patient_groups)) %>%
-      add_col_plot(y = col_order,
-                   tracename = "pseudotime",
-                   layout = list(title = "Pseudotime")) %>%
-      identity()
-
-  } else {
-    module_heatmap <- iheatmapr::iheatmap(as.matrix(agg_mat), col_labels = TRUE, row_labels = TRUE, cluster_rows = "hclust", cluster_cols = "hclust")
+  groups.use.numeric <- groups.use[sapply(groups.use, is.numeric)]
+  ha_cols.numeric <- NULL
+  if (length(groups.use.numeric) > 0) {
+    numeric_col_fun = function(myvec, color) {
+      circlize::colorRamp2(range(myvec), c("white", color))
+    }
+    ha_col_names.numeric <- names(groups.use.numeric)
+    ha_col_hues.numeric <- (scales::hue_pal())(length(ha_col_names.numeric))
+    ha_cols.numeric <- purrr::map2(groups.use[ha_col_names.numeric],
+                                   ha_col_hues.numeric, numeric_col_fun)
   }
+  ha_cols <- c(ha_cols.factor, ha_cols.numeric)
+  column_ha = ComplexHeatmap::HeatmapAnnotation(df = groups.use,
+                                                height = unit(group.bar.height, "points"), col = ha_cols)
 
+  module_heatmap <- ComplexHeatmap::Heatmap(as.matrix(agg_mat), name = "log expression",
+                                            top_annotation = column_ha,
+                                            left_annotation = row_ha,
+                                            cluster_columns = cluster_columns,
+                                            cluster_rows = cluster_rows,
+                                            show_column_names = FALSE,
+                                            column_dend_height = unit(mm_col_dend, "mm"),
+                                            # column_split = column_split,
+                                            column_title = NULL,
+                                            ...)
 
+  # module_heatmap <- iheatmapr::iheatmap(as.matrix(agg_mat), col_labels = TRUE, row_labels = TRUE, cluster_rows = "hclust", cluster_cols = NULL)
 
   return(list(module_table = gene_module_df, module_heatmap = module_heatmap, agg_mat = agg_mat))
 }
