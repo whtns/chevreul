0f9da114823af87131df5f0260d729e00451ad98
whtns <kevin.stachelek@gmail.com>
2024-03-24 22:07:18 -0700

    copy annotable code

---
+++ R/grch38.R
@@ -0,0 +1,26 @@
+#' Human annotation data
+#'
+#' Human (*Homo sapiens*) annotations based on
+#' genome assembly GRCH38 from Ensembl.
+#'
+#' @docType data
+#' @keywords datasets
+#'
+#' @details
+#' Variables:
+#'
+#' - ensgene
+#' - entrez
+#' - symbol
+#' - chr
+#' - start
+#' - end
+#' - strand
+#' - biotype
+#' - description
+#'
+#' @source \url{http://ensembl.org/homo_sapiens}
+#'
+#' @examples
+#' head(grch38)
+"grch38"
---
+++ R/grch38_tx2gene.R
@@ -0,0 +1,20 @@
+#' Human transcripts to genes
+#'
+#' Lookup table for converting Human (*Homo sapiens*)
+#' Ensembl transcript IDs to gene IDs based on genome assembly
+#' GRCH38 from Ensembl.
+#'
+#' @docType data
+#' @keywords datasets
+#'
+#' @details
+#' Variables:
+#'
+#' - enstxp
+#' - ensgene
+#'
+#' @source \url{http://ensembl.org/homo_sapiens}
+#'
+#' @examples
+#' head(grch38_tx2gene)
+"grch38_tx2gene"
